b4c5c70bde15e900d4cb39e512a6e6be8ef7fcc4
sjchin88 <jinqwem@hotmail.com>
2023-10-31 01:49:00 -0700

    interim

---
+++ Project4/displayAR.cpp
@@ -0,0 +1,201 @@
+#include <opencv2/core.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include "utils.h"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+    // check for sufficient arguments
+    if (argc < 2)
+    {
+        printf("usage: %s <cameraIdx> <defaultSavePath>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Getting the variables from command line
+    // argv[1] = cameraIdx, argv[2] = defaultSavePath,
+    // Set the cameraIdx from argv[1], and set the videocapture element
+    VideoCapture *capdev;
+    int cameraIdx;
+    try
+    {
+        cameraIdx = std::stoi(argv[1]);
+    }
+    catch (std::exception)
+    {
+        // Set it to 0
+        cameraIdx = 0;
+    }
+    cout << "camera idx is " << cameraIdx << endl;
+
+    // open the video device
+    capdev = new cv::VideoCapture(cameraIdx);
+    if (!capdev->isOpened())
+    {
+        printf("Unable to open video device\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // get some properties of the image
+    cv::Size refS((int)capdev->get(cv::CAP_PROP_FRAME_WIDTH),
+                  (int)capdev->get(cv::CAP_PROP_FRAME_HEIGHT));
+    printf("Expected size: %d %d\n", refS.width, refS.height);
+    int fps = (int)capdev->get(cv::CAP_PROP_FPS);
+    printf("Captured fps: %d \n", fps);
+
+    // Set default fps for saving
+    int savingFPS = 10;
+    if (fps > 0)
+    {
+        savingFPS = fps;
+    }
+
+    // Set default save path from argv[2]
+    String defaultSavePath;
+    try
+    {
+        defaultSavePath = argv[2];
+    }
+    catch (std::exception)
+    {
+        std::cout << "Could not read the default save path " << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    cout << "save path is " << defaultSavePath << endl;
+
+    Mat capturedFrame;
+    Mat tempFrame;
+    Mat displayFrame;
+
+    // Initialize all boolean flags for display option
+    bool demo = false;
+    bool videoSaving = false;
+
+    cv::VideoWriter videoWriter;
+    // Read the camera matrix and dist coeffs from file
+    Mat cameraMatrix;
+    Mat distCoeffs;
+    readIntrinsic(cameraMatrix, distCoeffs, defaultSavePath);
+    Mat rvec;
+    Mat tvec;
+    vector<cv::Vec3f> point_set;
+    vector<Point2f> corner_set; // this will be filled by the detected corners
+    for (int y = 0; y < 6; y++)
+    {
+        for (int x = 0; x < 9; x++)
+        {
+            cv::Vec3f tempPt = Vec3f(x, y, 0);
+            point_set.push_back(tempPt);
+        }
+    }
+
+    vector<cv::Vec3f> objectPoints;
+    objectPoints.push_back(Vec3f(4.5, 2.5, 0));  // Center
+    objectPoints.push_back(Vec3f(9, 2.5, 0));    // x-edge
+    objectPoints.push_back(Vec3f(4.5, 6, 0));    // y-edge
+    objectPoints.push_back(Vec3f(4.5, 2.5, -5)); // z-edge
+    vector<Point2f> arPoints;
+
+    // Run the while loop
+    while (true)
+    {
+        *capdev >> capturedFrame; // get a new frame from the camera, treat as a stream
+
+        if (capturedFrame.empty())
+        {
+            printf("frame is empty\n");
+            break;
+        }
+
+        // Default is to set the displayFrame as capturedFrame
+        displayFrame = capturedFrame;
+        Mat gray; // source image
+        cv::cvtColor(capturedFrame, gray, cv::COLOR_BGR2GRAY);
+        Size patternsize(9, 6); // interior number of corners, columns, rows
+
+        // CALIB_CB_FAST_CHECK saves a lot of time on images
+        // that do not contain any chessboard corners
+        bool patternfound = findChessboardCorners(gray, patternsize, corner_set,
+                                                  CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + CALIB_CB_FAST_CHECK);
+        if (patternfound)
+        {
+            cornerSubPix(gray, corner_set, Size(11, 11), Size(-1, -1),
+                         TermCriteria(cv::TermCriteria::Type::EPS + cv::TermCriteria::Type::MAX_ITER, 30, 0.1));
+            solvePnP(point_set, corner_set, cameraMatrix, distCoeffs, rvec, tvec);
+            if (demo)
+            {
+                cout << "rvec: " << endl
+                     << rvec << endl;
+                cout << "tvec: " << endl
+                     << tvec << endl;
+                drawChessboardCorners(displayFrame, patternsize, Mat(corner_set), patternfound);
+            }
+            projectPoints(objectPoints, rvec, tvec, cameraMatrix, distCoeffs, arPoints);
+            cv::arrowedLine(displayFrame, arPoints[0], arPoints[1], Scalar(255, 0, 0), 5);
+            cv::arrowedLine(displayFrame, arPoints[0], arPoints[2], Scalar(0, 255, 0), 5);
+            cv::arrowedLine(displayFrame, arPoints[0], arPoints[3], Scalar(0, 0, 255), 5);
+        }
+
+        // check if video saving is on
+        if (videoSaving)
+        {
+            videoWriter.write(displayFrame);
+        }
+
+        cv::imshow("Video", displayFrame);
+
+        // see if there is a waiting keystroke every 33ms
+        char key = cv::waitKey(33);
+
+        switch (key)
+        {
+
+        //  keystroke v turn on saving video to the file
+        case 'v':
+        {
+            // if videoSaving is on, turn it off
+            if (videoSaving)
+            {
+                videoWriter.release();
+                videoSaving = false;
+            }
+            else
+            {
+                // Set the video writer
+                std::time_t timeStamp = std::time(nullptr);
+                String videoPath = defaultSavePath + "video_" + to_string(timeStamp) + ".avi";
+                /*  cv::VideoWriter::VideoWriter (const String &filename, int fourcc, double fps,Size frameSize,bool isColor = true
+                        )	 */
+                videoWriter = cv::VideoWriter(videoPath, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), savingFPS, refS);
+                videoSaving = true;
+            }
+
+            break;
+        }
+
+        default:
+            break;
+        }
+
+        // keystroke s save the current display frame
+        if (key == 's')
+        {
+            std::time_t timeStamp = std::time(nullptr);
+            String finalPath = defaultSavePath + "screenshot_" + to_string(timeStamp) + ".png";
+            imwrite(finalPath, displayFrame);
+        }
+
+        // keystroke q quit the program
+        if (key == 'q')
+        {
+            break;
+        }
+    }
+
+    delete capdev;
+    return (0);
+}
---
+++ Project4/utils.cpp
@@ -0,0 +1,89 @@
+#include "utils.h"
+
+int calibrateNSave(Mat &frame, vector<vector<cv::Vec3f>> &point_list, vector<vector<cv::Point2f>> &corner_list, String saveDir)
+{
+    try
+    {
+        if (point_list.size() < 5)
+        {
+            printf("Not enough data for calibration\n");
+            return (-1);
+        }
+        if (point_list.size() != corner_list.size())
+        {
+            printf("Data error, size of point_list != size of corner_list");
+            return (-1);
+        }
+        Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
+        cameraMatrix.at<double>(0, 2) = frame.cols / 2;
+        cameraMatrix.at<double>(1, 2) = frame.rows / 2;
+        Mat distCoeffs = cv::Mat::zeros(8, 1, CV_64F);
+        vector<Mat> rvecs;
+        vector<Mat> tvecs;
+        cout << "camera matrix before calibration " << endl
+             << cameraMatrix << endl;
+        cout << "dist coefficient before calibration " << endl
+             << distCoeffs << endl;
+        double reprojectionError = cv::calibrateCamera(point_list, corner_list, frame.size(), cameraMatrix, distCoeffs, rvecs, tvecs, cv::CALIB_FIX_ASPECT_RATIO);
+        cout << "camera matrix after calibration " << endl
+             << cameraMatrix << endl;
+        cout << "dist coefficient after calibration " << endl
+             << distCoeffs << endl;
+        cout << "Re-projection error reported by calibrateCamera: " << reprojectionError << endl;
+        if (saveIntrinsic(cameraMatrix, distCoeffs, saveDir) != 0)
+        {
+            return (-1);
+        };
+    }
+    catch (exception)
+    {
+        cout << "Error in calibration" << endl;
+        return (-1);
+    }
+
+    return 0;
+}
+
+int saveIntrinsic(Mat &cameraMatrix, Mat distCoeffs, string saveDir)
+{
+    try
+    {
+        string filename = saveDir + "cameraIntrinsic.xml";
+        FileStorage fs(filename, FileStorage::WRITE);
+        fs << "CameraMatrix" << cameraMatrix;
+        fs << "DistortionCoeffs" << distCoeffs;
+        fs.release();
+        cout << "Write Done." << endl;
+    }
+    catch (exception)
+    {
+        cout << "Error in saving" << endl;
+        return (-1);
+    }
+
+    return 0;
+}
+
+int readIntrinsic(Mat &cameraMatrix, Mat distCoeffs, string saveDir)
+{
+    try
+    {
+        string filename = saveDir + "cameraIntrinsic.xml";
+        FileStorage fs(filename, FileStorage::READ);
+        fs["CameraMatrix"] >> cameraMatrix;
+        fs["DistortionCoeffs"] >> distCoeffs;
+        fs.release();
+        cout << "Reading Done." << endl;
+        cout << "Camera Matrix" << endl
+             << cameraMatrix << endl;
+        cout << "Distortion Coeffs" << endl
+             << distCoeffs << endl;
+    }
+    catch (exception)
+    {
+        cout << "Error in saving" << endl;
+        return (-1);
+    }
+
+    return 0;
+}
